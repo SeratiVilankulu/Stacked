c782af06492b9c41fc99dfd9b06ff460b6b88b67
SeratiVilankulu <seratimotla@gmail.com.com>
2026-09-14 21:19:51 +0200

    Add login endpoint with password verification

--- stacked_backend/src/controllers/authController.js
+++ stacked_backend/src/controllers/authController.js
@@ -53,4 +53,39 @@ async function register(req, res) {
 	}
 }
 
-export { register };
+async function login(req, res) {
+	const { email, password } = req.body;
+	try {
+		if (!email || !password) {
+			return res
+				.status(400)
+				.json({ message: "Email and password are required" });
+		}
+
+		// Check if user email exists
+		const user = await db.orm.public.User.where({
+			email,
+		}).first();
+
+		// If user doesn't exist
+		if (!user) {
+			return res.status(401).json({ error: "Invalid email or password" });
+		}
+
+		// Verify password
+		const isPasswordValid = await bcrypt.compare(password, user.password);
+
+		if (!isPasswordValid) {
+			return res.status(401).json({ error: "Invalid email or password" });
+		}
+
+		// Success
+		res.status(200).json({ message: "User successfully logged in" });
+	} catch (error) {
+		return res.status(500).json({
+			message: error.message,
+		});
+	}
+}
+
+export { register, login };
